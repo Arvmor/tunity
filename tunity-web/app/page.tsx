12c0334046235673fa5b8daee638fe9b14761941
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-26 23:54:42 -0500

    feat: Add mockup trending page

--- tunity-web/app/page.tsx
+++ tunity-web/app/page.tsx
@@ -1,8 +1,51 @@
+import AppItem, { ItemProps } from "@/components/app/item";
+
+const trending: { title: string, items: ItemProps[] }[] = [
+  {
+    title: "Trending Songs",
+    items: [
+      {
+        title: "Song 1",
+        name: "Artist 1",
+        image: "/placeholder.jpg",
+      },
+    ],
+  },
+  {
+    title: "Popular Artists",
+    items: [
+      {
+        title: "Podcast 1",
+        name: "Artist 1",
+        image: "/placeholder.jpg",
+      },
+    ]
+  },
+  {
+    title: "Featured",
+    items: [
+      {
+        title: "Podcast 1",
+        name: "Artist 1",
+        image: "/placeholder.jpg",
+      },
+    ]
+  },
+]
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full flex-col justify-between py-32 px-16">
         {/* Content */}
+        {trending.map(({ title, items }) => (
+          <div key={title} className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">{title}</h1>
+            {items.map(({title, name, image}) => (
+              <AppItem key={title} title={title} name={name} image={image} />
+            ))}
+          </div>
+        ))}
       </main>
     </div>
   );
