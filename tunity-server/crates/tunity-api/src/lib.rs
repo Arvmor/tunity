2833d188ba42ce9d6f6863aa694030afce44a967
Arvin Moravej <arvin.morawej@gmail.com>
2025-11-30 13:36:39 -0500

    chore: Add player module

--- tunity-server/crates/tunity-api/src/lib.rs
+++ tunity-server/crates/tunity-api/src/lib.rs
@@ -1,9 +1,11 @@
 mod health;
+mod player;
 mod server;
 mod utils;
 mod x402;
 
 pub use health::HealthRoute;
+pub use player::PlayerRoute;
 pub use server::Server;
 pub use utils::ResultAPI;
 pub use x402::{ConfigX402, FacilitatorRequest, FacilitatorResponse};
---
+++ tunity-server/crates/tunity-api/src/player.rs
@@ -0,0 +1,6 @@
+/// The Player Routes
+#[derive(Debug)]
+pub enum PlayerRoute {
+    /// The play endpoint
+    Play,
+}
