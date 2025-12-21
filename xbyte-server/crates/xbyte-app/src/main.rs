0e9048b0672615859896e32a450d4b0c43f2709a
Arvin Morawej <arvin.morawej@gmail.com>
2025-12-20 22:46:17 -0500

    chore: read .env on dev mode

--- xbyte-server/crates/xbyte-app/src/main.rs
+++ xbyte-server/crates/xbyte-app/src/main.rs
@@ -2,6 +2,9 @@ use xbyte_api::Server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    #[cfg(debug_assertions)]
+    dotenv::dotenv().ok();
+
     let server = Server::new("127.0.0.1:80");
     server.run().await?;
 
