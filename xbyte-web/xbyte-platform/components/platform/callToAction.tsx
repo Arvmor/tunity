ebb191f6cd3bfc6765b10c427850a502e2c5d1d9
Arvin Moravej <moravej.arvin@gmail.com>
2025-12-24 18:15:17 -0500

    feat: Add CTA component

---
+++ xbyte-web/xbyte-platform/components/platform/callToAction.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+
+export interface CallToActionProps {
+    titleText: string;
+    descriptionText?: string;
+    buttonText?: string;
+    buttonAction?: () => void;
+}
+
+export default function CallToAction({
+    titleText,
+    descriptionText,
+    buttonText,
+    buttonAction,
+}: CallToActionProps) {
+    const description = descriptionText ? (
+        <h1 className="text-muted-foreground/80">{descriptionText}</h1>
+    ) : null;
+
+    const button = buttonText ? (
+        <Button size="sm" onClick={buttonAction}>
+            {buttonText}
+        </Button>
+    ) : null;
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-4">
+            <div className="text-3xl font-bold text-center">
+                <h1>{titleText}</h1>
+                {description}
+            </div>
+
+            {button}
+        </div>
+    );
+}
